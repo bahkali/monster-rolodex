75365b2c234b5abff525f55b41493047a0bc3de5
kaly <kalimamadou@gmail.com>
2020-05-07 20:24:38 -0700

    Updates

---
+++ precache-manifest.914520cb96fd501fa653fe1c7e205f48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e17933d27333cb67bd7ef1895d3776e4",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "90bfaf217576bbfafc5c",
+    "url": "/monster-rolodex/static/css/main.b98805bc.chunk.css"
+  },
+  {
+    "revision": "f7b84cd810271619eca9",
+    "url": "/monster-rolodex/static/js/2.7d94dc6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.7d94dc6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90bfaf217576bbfafc5c",
+    "url": "/monster-rolodex/static/js/main.3b8b4693.chunk.js"
+  },
+  {
+    "revision": "fdce5faec76031078f23",
+    "url": "/monster-rolodex/static/js/runtime-main.c9e8ecbd.js"
+  }
+]);
